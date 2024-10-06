887fa626b76aae803acd458fb499176bb581ac3b
anastasiya <nastya050620031n@gmail.com>
2024-10-06 23:18:38 +0700

    fixed style

--- fragment-navigation/app/src/main/java/ru/nsu/fragment/navigation/FragmentB.kt
+++ fragment-navigation/app/src/main/java/ru/nsu/fragment/navigation/FragmentB.kt
@@ -9,10 +9,14 @@ import androidx.fragment.app.Fragment
 
 class FragmentB : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_b, container, false)
 
-        val toCButton : Button = view.findViewById(R.id.toCButton)
+        val toCButton: Button = view.findViewById(R.id.toCButton)
         toCButton.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, FragmentC())
