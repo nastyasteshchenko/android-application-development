1cecd1234ccf2ec1fef412d6df49b623fcdc340f
anastasiya <nastya050620031n@gmail.com>
2024-10-08 21:31:09 +0700

    updated MainActivity onCreate method

--- recycler-view/app/src/main/java/ru/nsu/recycler/view/MainActivity.kt
+++ recycler-view/app/src/main/java/ru/nsu/recycler/view/MainActivity.kt
@@ -1,20 +1,38 @@
 package ru.nsu.recycler.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.facebook.drawee.backends.pipeline.Fresco
+import ru.nsu.recycler.view.adapter.PlayerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val VERTICAL_SPACE_HEIGHT_DP = 16
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Fresco.initialize(this)
+
+        val adapter = PlayerAdapter()
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        val spaceDecoration =
+            SpaceItemDecoration(
+                (VERTICAL_SPACE_HEIGHT_DP * resources.displayMetrics.density)
+                    .toInt()
+            )
+        recyclerView.addItemDecoration(spaceDecoration)
+
+        val test = Test()
+        test.startTest(adapter)
     }
 }
