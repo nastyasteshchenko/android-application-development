27cc6ac089093079826092c04fcc086c504b29be
anastasiya <nastya050620031n@gmail.com>
2024-12-09 16:27:46 +0700

    style fix

--- currency-converter-2/app/src/main/java/ru/nsu/currency/converter/domain/usecase/RefreshCurrenciesUseCase.kt
+++ currency-converter-2/app/src/main/java/ru/nsu/currency/converter/domain/usecase/RefreshCurrenciesUseCase.kt
@@ -3,7 +3,9 @@ package ru.nsu.currency.converter.domain.usecase
 import ru.nsu.currency.converter.domain.repository.CurrencyRepository
 import javax.inject.Inject
 
-class RefreshCurrenciesUseCase @Inject constructor(private val currencyRepository: CurrencyRepository) {
+class RefreshCurrenciesUseCase @Inject constructor(
+    private val currencyRepository: CurrencyRepository
+) {
 
     suspend fun invoke() {
         currencyRepository.refreshCurrencies()
